1e774d2d641cf68cb3769707f7ac9b14cbb7b9f4
darftsoftware <33342164-darftsoftware@users.noreply.replit.com>
2024-08-01 17:15:47 +0000

    AprN1

--- myApp/public/scripts/search.js
+++ myApp/public/scripts/search.js
@@ -1,79 +1,159 @@
-// document.getElementById('searchForm').addEventListener('submit', function(event) {
-//   const searchTerm = document.getElementById('searchInput').value;
-//   if (!searchTerm) {
-//     event.preventDefault();
-//     return;
-//   }
-//   this.action = `api/dogs/${searchTerm}`;
-// });
+// https://www.canva.com/design/DAGDO1AYqMk/caOo9sz8mz-GCZPRtiOZgg/edit?utm_content=DAGDO1AYqMk&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton
+//
+// Fazer um css decente pro autocomplete (Tá paia pq não consigo colocar background)
+// Css do solodog de os campos serem alternados por um normal e outro com o bg cinza!!
+//
 
 
+// Barra de Pesquisa
+const inp = document.getElementById("searchInput");
+const results = document.getElementById('res');
 
-document.addEventListener('DOMContentLoaded', function() {
+// evento quando clica na barra de pesquisa (POGGERS!)
+inp.addEventListener("focusin", () => {
+  /*
+  fetch(`/api/dogs`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((dogs) => {
+
+  })
+  */
+
+  results.style.display = "inline";
+  results.style.marginTop = "3px";
+  results.style.backgroundColor = "red";
+  
+});
+
+// Script que desativa os pre set de pesquisa quando clicka fora da barra de pesquisa (POGGERS!!!)
+
+inp.addEventListener("focusout", (event) => {
+  setTimeout( () => {
+      document.getElementById("res").style.display = "none";
+      inp.value = "";
+  } , 150)
+});
+
+inp.addEventListener("input", (e) => {
+  // Evento quando digita no search bar
+
+  // value é oq ta sendo digitado
+  let val = e.target.value;
+
+  // Script pra primeira letra digitar ser maiscula, mesmo após o espaço (POGGERS!!!)
+  let element = e.target;
+  var words = element.value.split(' ');
+  element.value = words.map(w => w.slice(0,1).toUpperCase() + w.slice(1)).join(' ');
+
+  fetch(`/api/dogs/six/${val}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((dogs) => {
+    results.innerHTML = '';
+    dogs.forEach(dog => {
+      const li = document.createElement('li');
+      
+      li.onclick = () => {
+        console.log('A')
+        displayDogInfo(dog);
+      };
+
+      li.addEventListener("mouseover" , () => {
+        li.style.cursor = "pointer";
+      })
+
+      li.textContent = dog.nome;
+      results.appendChild(li);
+        
+    })
+    
+  });
+
+  /*
+  results.style.color = "black";
+  results.style.backgroundColor = "white";
+  results.style.overflow = "auto";
+  */
+  
+});
+
+document.addEventListener("DOMContentLoaded", function () {
   const params = new URLSearchParams(window.location.search);
-  const searchTerm = params.get('nome');
+  const searchTerm = params.get("nome");
+  //const searchTerm = document.getElementById("searchInput").value;
 
   if (searchTerm) {
     fetch(`/api/dogs/${searchTerm}`)
-      .then(response => {
+      .then((response) => {
         if (!response.ok) {
-          throw new Error('Cão não encontrado');
+          throw new Error("Cão não encontrado");
         }
         return response.json();
       })
-      .then(dog => {
+      .then((dog) => {
         displayDogInfo(dog);
       })
-      .catch(error => {
+      .catch((error) => {
         displayError(error.message);
       });
   }
-
- 
 });
 
 function displayDogInfo(dog) {
-  document.getElementById('nome').innerText = dog.nome;
-  document.getElementById('tamanho').innerText = dog.tamanho;
-  document.getElementById('peso').innerText = dog.peso;
-  document.getElementById('corPelagem').innerText = dog.corPelagem;
-  document.getElementById('tipoPelagem').innerText = dog.tipoPelagem;
-  document.getElementById('expectativa').innerText = dog.expectativaVida;
-  document.getElementById('energia').innerText = dog.nivelEnergia;
-  document.getElementById('temperamento').innerText = dog.temperamento;
-  document.getElementById('treinamento').innerText = dog.facilidadeTreinamento;
-  document.getElementById('doencas').innerText = dog.propensaoDoencasGeneticas;
-  document.getElementById('cuidadoPelagens').innerText = dog.necessidadesCuidadosPelagem;
-  document.getElementById('exercicios').innerText = dog.necessidadesExercicio;
-  document.getElementById('requisitoEspaco').innerText = dog.requisitosEspaco;
-  document.getElementById('socializacao').innerText = dog.nivelSocializacao;
-  document.getElementById('protecao').innerText = dog.nivelProtecao;
-  document.getElementById('latido').innerText = dog.nivelLatido;
-  document.getElementById('compra').innerText = dog.precoMedioCompra;
-  document.getElementById('medico').innerText = dog.custosVeterinariosMensais;
-  document.getElementById('alimentacao').innerText = dog.custosAlimentacaoMensais;
-  document.getElementById('descricao').innerText = dog.descricao;
+  document.getElementById("dogphoto").src = dog.image;
+  document.getElementById("dogphoto").alt = dog.nome;
+  document.getElementById("nome").innerText = dog.nome;
+  document.getElementById("tamanho").innerText = dog.tamanho;
+  document.getElementById("peso").innerText = dog.peso;
+  document.getElementById("corPelagem").innerText = dog.corPelagem;
+  document.getElementById("tipoPelagem").innerText = dog.tipoPelagem;
+  document.getElementById("expectativa").innerText = dog.expectativaVida;
+  document.getElementById("energia").innerText = dog.nivelEnergia;
+  document.getElementById("temperamento").innerText = dog.temperamento;
+  document.getElementById("treinamento").innerText = dog.facilidadeTreinamento;
+  document.getElementById("doencas").innerText = dog.propensaoDoencasGeneticas;
+  document.getElementById("cuidadoPelagens").innerText = dog.necessidadesCuidadosPelagem;
+  document.getElementById("exercicios").innerText = dog.necessidadesExercicio;
+  document.getElementById("requisitoEspaco").innerText = dog.requisitosEspaco;
+  document.getElementById("socializacao").innerText = dog.nivelSocializacao;
+  document.getElementById("protecao").innerText = dog.nivelProtecao;
+  document.getElementById("latido").innerText = dog.nivelLatido;
+  document.getElementById("compra").innerText = dog.precoMedioCompra;
+  document.getElementById("medico").innerText = dog.custosVeterinariosMensais;
+  document.getElementById("alimentacao").innerText = dog.custosAlimentacaoMensais;
+  document.getElementById("descricao").innerText = dog.descricao;
 }
 
 function displayError(message) {
-  window.alert("cão não encontrado")
+  window.alert("cão não encontrado");
 }
- 
-    
 
-    document.getElementById('searchCompara').addEventListener('submit', function (event) {
-
+document.getElementById("searchCompara").addEventListener("submit", function (event) {
     event.preventDefault();
 
     //const searchDog = document.getElementById('nome').value;
-    const searchDog = new URLSearchParams(window.location.search).get('nome');
-    const searchCompara = document.getElementById('compara').value;
-    
-   window.location.href = window.location.href = `/compara?dog=${searchDog}&compara=${searchCompara}`;
-  
+    const searchDog = new URLSearchParams(window.location.search).get("nome");
+    const searchCompara = document.getElementById("compara").value;
 
+    window.location.href =
+      window.location.href = `/compara?dog=${searchDog}&compara=${searchCompara}`;
   });
 
 
+//
+// Area de codigo teste 
+//
 
-
+/*
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+  const searchTerm = document.getElementById('searchInput').value;
+  if (!searchTerm) {
+    event.preventDefault();
+    return;
+  }
+  this.action = `api/dogs/${searchTerm}`;
+});
+*/
--- myApp/routes/searchRouter.js
+++ myApp/routes/searchRouter.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router()
 const { MongoClient } = require('mongodb');
 
-
-
-
 const uri = "mongodb+srv://darftsoftware:11101110@cluster0.8en20si.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -21,6 +18,36 @@ client.connect(err => {
     try {
       const dogs = await collection.find({}).toArray();
       res.json(dogs);
+      //res.toArray(dogs);
+      //return await dogs.toArray()
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  router.get('/dogs/six/:userInput', async (req, res) => {
+    /*
+      const query = req.query.q;
+      const collection = db.collection('books');
+
+      collection.find({ title: new RegExp(query, 'i') }).toArray((err, docs) => {
+          if (err) throw err;
+          res.json(docs);
+      });
+    */
+    try {
+      //db.usuarios.find({ nome: { $regex: userInput, $options: 'i' } })
+      /*
+      const userInput = req.params.nome;
+      const dogs = await collection.find({ nome: { $regex: userInput } }).toArray();
+      res.json(dogs);
+      */
+      const userInput = req.params.userInput;
+      collection.find({ nome: { $regex: userInput } }).toArray((err, docs) => {
+        if (err) throw err;
+        res.json(docs);
+      });
+      
     } catch (err) {
       res.status(500).send(err);
     }
